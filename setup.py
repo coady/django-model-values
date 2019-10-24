1d8a108dc4d90bb867be1d0f9f586165be0a6883
Aric Coady <aric.coady@gmail.com>
2019-10-23 17:36:28 -0700

    Python 3.8 verified.

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Database :: Database Engines/Servers',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
