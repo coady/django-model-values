511b9216c9d30c5d6b651802e8b2b2e4fef2f1d1
Aric Coady <aric.coady@gmail.com>
2024-06-29 10:28:28 -0700

    Pytest plugins are automatically found.
    The django plugin reports its version.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,5 @@
-import django
 from django.conf import settings
 
-
-def pytest_report_header(config):
-    return 'Django ' + django.__version__
-
-
-pytest_plugins = ('django',)
 settings.configure(
     INSTALLED_APPS=['tests'], DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3'}}
 )
